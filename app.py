493692ba7bf050dc86273c9d6c6b7bfe1f1028e7
zeeshanvirani <zvirani99@gmail.com>
2022-04-11 20:16:19 -0500

    idk

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def upload_files():
     filename = uploaded_file.filename
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
+        # if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+        #     abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return render_template("story.html")
 
